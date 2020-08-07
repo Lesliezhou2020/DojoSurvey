2f8915f3b29842faa5679fa603c450d49be87e44
zhou yunzhuo <leslie@zhoudeMacBook-Air.local>
2020-08-06 22:56:04 -0700

    Dojo Survey Project

---
+++ DjangoSurvey/DjangoAPP/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+# Create your views here.\
+def root(request):
+    if request.method == 'GET':
+        return render(request, "index.html")
+    
+ 
+def submission(request):
+    if request.method == 'POST':
+        language = request.POST['language']
+        name = request.POST['name']
+        location = request.POST['Location']
+        Comments =request.POST['Comments']
+        context = {
+    	    "name_on_template" : name,
+    	    "location_on_template" : location,
+            "language_on_template" : language,
+            "comments_on_template" : Comments,
+        }      
+    return render(request, 'result.html', context)
